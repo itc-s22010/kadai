34f2d1896f487ec27387f7cbd47f2a0f9c03ddf0
itc-s22010 <s22010@std.it-college.ac.jp>
2023-01-19 19:38:00 +0000

    commit

---
+++ pages/index.js
@@ -0,0 +1,35 @@
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href="/page1">
+            page1
+          </Link>
+        </li>
+        <li>
+          <Link href="/page2">
+            page2
+          </Link>
+        </li>
+        <li>
+          <Link href="/page3">
+            page3
+          </Link>
+        </li>
+        <li>
+          <Link href="/page4">
+            page4
+          </Link>
+        </li>
+        <li>
+          <Link href="/page5">
+            page5
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
